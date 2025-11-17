fe94864a6866a1a4a65a75a35d1374725cfcac49
ddluan <luandd0410@pm.me>
2025-11-18 00:43:23 +0700

    Delete ldd.bat

--- ldd.bat
+++
@@ -1,5 +0,0 @@
-sed -i "s/templatetoaster.com/luandd.net/g" index.html
-sed -i "s/TemplateToaster.com/luandd.net/g" index.html
-sed -i "s/mycv/MyCV/g" index.html
-sed -i "s/luandd_cv/LuanDD_CV/g" index.html
-copy index.html home.html
